2f51946dc9ee31b9a9152af2733b385eb8a45660
irokaru <karuta@nononotyaya.net>
2021-10-31 17:46:42 +0900

    create textarea component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div>hello world</div>
+  <FormSelect name="test" />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import FormSelect from "@/components/Textarea.vue";
 
-@Options({})
+@Options({
+  components: {
+    FormSelect,
+  },
+})
 export default class App extends Vue {}
 </script>
---
+++ src/components/Textarea.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="form-part">
+    <label :for="name" v-html="label" v-if="label"></label>
+
+    <textarea :name="name"></textarea>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    name: { type: String, require: true },
+    label: { type: String, require: false },
+  },
+})
+export default class Textarea extends Vue {
+  name!: string;
+}
+</script>
