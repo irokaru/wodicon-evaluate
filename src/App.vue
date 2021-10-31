95658c0b87423cfc69903e1ec61f56b3c4a74614
irokaru <karuta@nononotyaya.net>
2021-10-31 23:39:09 +0900

    add evaluate table row

--- src/App.vue
+++ src/App.vue
@@ -14,24 +14,67 @@
     <div class="right">
       <div class="btn big soft orange" @click="exec">評価を算出</div>
     </div>
+
+    <hr />
+
+    <h2>算出結果</h2>
+
+    <div>投票数: {{ evaluates.length }}</div>
+
+    <table>
+      <thead>
+        <th>投票者名</th>
+        <th>熱中</th>
+        <th>斬新</th>
+        <th>物語</th>
+        <th>画像音声</th>
+        <th>遊びやすさ</th>
+        <th>その他</th>
+      </thead>
+
+      <tbody>
+        <EvaluateTr
+          :evaluate="evaluate"
+          v-for="(evaluate, index) in evaluates"
+          :key="`${evaluate.name}-${index}`"
+        />
+      </tbody>
+
+      <tfoot>
+        <th>投票者名</th>
+        <th>熱中</th>
+        <th>斬新</th>
+        <th>物語</th>
+        <th>画像音声</th>
+        <th>遊びやすさ</th>
+        <th>その他</th>
+      </tfoot>
+    </table>
   </main>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+
 import FormSelect from "@/components/Textarea.vue";
+import EvaluateTr from "@/components/EvaluateTr.vue";
+
+import { EvaluateRow } from "./interfaces/EvaluateRow";
+import { text2EvaluateRowArray } from "./lib/EvaluateText";
+import { Evaluates } from "./interfaces/Evaluates";
 
 @Options({
   components: {
     FormSelect,
+    EvaluateTr,
   },
 })
 export default class App extends Vue {
   public text = "";
+  public evaluates: EvaluateRow[] = [];
 
   public exec(): void {
-    // TODO
-    console.log("clicked!!!");
+    this.evaluates = text2EvaluateRowArray(this.text);
   }
 }
 </script>
---
+++ src/components/EvaluateTr.vue
@@ -0,0 +1,25 @@
+<template>
+  <tr>
+    <td>{{ evaluate.name }}</td>
+    <td>{{ evaluate.score.enthusiasm }}</td>
+    <td>{{ evaluate.score.innovative }}</td>
+    <td>{{ evaluate.score.story }}</td>
+    <td>{{ evaluate.score.media }}</td>
+    <td>{{ evaluate.score.easy }}</td>
+    <td>{{ evaluate.score.other }}</td>
+  </tr>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { EvaluateRow } from "@/interfaces/EvaluateRow";
+
+@Options({
+  props: {
+    evaluate: { type: Object, required: true },
+  },
+})
+export default class EvaluateTr extends Vue {
+  public evaluate!: EvaluateRow;
+}
+</script>
