18ee4ebf7adea4aac15e619427370acad48f3559
irokaru <karuta@nononotyaya.net>
2021-10-31 17:25:19 +0900

    add style base

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,3 @@ import { Options, Vue } from "vue-class-component";
 @Options({})
 export default class App extends Vue {}
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import "./scss/app.scss";
+
+// --------------------------------------------------------------------
+
 createApp(App).mount("#app");
