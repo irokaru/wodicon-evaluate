4cd1cb496d9e2fa86697bddc1b33c0cae5394f28
irokaru <karuta@nononotyaya.net>
2021-11-01 12:36:19 +0900

    add textarea test

---
+++ tests/unit/components/Textarea.spec.ts
@@ -0,0 +1,77 @@
+import { mount } from "@vue/test-utils";
+
+import Textarea from "@/components/Textarea.vue";
+
+// --------------------------------------------------------------------
+
+describe("input", () => {
+  test("propsの確認", async () => {
+    const suites = [
+      // expect, input
+      ["", ""],
+      ["aaa\naaa", "aaa\naaa"],
+    ];
+
+    for (const suite of suites) {
+      const props = {
+        name: "a",
+        modelValue: "",
+      };
+
+      const wrapper = mount(Textarea, {
+        props: props,
+      });
+
+      await wrapper.get("textarea").setValue(suite[1]);
+      await wrapper.vm.$nextTick();
+
+      const emit = wrapper.emitted("update:modelValue") as any[][];
+
+      expect(emit).toBeTruthy();
+      expect(emit[0][0]).toEqual(suite[0]);
+    }
+  });
+});
+
+describe("labelText", () => {
+  test("アスタリスクの出力確認", () => {
+    const suites: [string, string, boolean][] = [
+      // expect, label, required,
+      [
+        '<label for="a">aaaaa<span class="required">*</span></label>',
+        "aaaaa",
+        true,
+      ],
+      ['<label for="a">aaaaa</label>', "aaaaa", false],
+    ];
+
+    for (const suite of suites) {
+      const props = {
+        name: "a",
+        label: suite[1],
+        modelValue: "",
+        required: suite[2],
+      };
+
+      const wrapper = mount(Textarea, {
+        props: props,
+      });
+
+      expect(wrapper.get("label").html()).toEqual(suite[0]);
+    }
+  });
+
+  test("labelの指定がないときには表示されないこと", () => {
+    const props = {
+      name: "hogehoge",
+      modelValue: "",
+      required: true,
+    };
+
+    const wrapper = mount(Textarea, {
+      props: props,
+    });
+
+    expect(wrapper.find("label").exists()).toBeFalsy();
+  });
+});
